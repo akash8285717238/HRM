39436f55ee0c1d32cd065e20eec37c33254d35f4
AKII <AKII@AKII>
2022-11-22 12:17:27 +0530

    GitMsg send by akash

---
+++ src/ClassGit/Class1.java
@@ -0,0 +1,12 @@
+package ClassGit;
+
+public class Class1 
+{
+public static void main (String[] args) 
+{
+	System.out.println("--------------GitPush--------");
+	System.out.println("--------------GitPull--------");
+	
+}
+
+}
